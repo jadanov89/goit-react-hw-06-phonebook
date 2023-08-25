5d9f5fed753e476cbb302a224678a910fa35f1af
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-08-25 22:05:03 +0300

    fix-0.3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,16 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Form  from './Form/Form';
+import Form from './Form/Form';
 import { Filter } from './Filter/Filter';
-import { List } from './List/List'; // Додав імпорт
-import { getContacts } from '../Redux/Contacts/contacts-selector';
-import { getFilter } from '../Redux/Filter/filter-selector';
-import { addContact, deleteContact } from '../Redux/Contacts/contacts-slice';
-
+import List from './List/List';
 
 export default function App() {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-  const dispatch = useDispatch();
-
-  const onAddContact = contact => {
-    const action = addContact(contact);
-    dispatch(action);
-  };
-
-  const onDeleteContact = contactId => {
-    const action = deleteContact(contactId);
-    dispatch(action);
-  };
-
-  // Видаліть filterContacts, оскільки ви його не використовуєте
-
-  const getFilteredContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <Form addContact={onAddContact} />
+      <Form />
       <h2>Contacts</h2>
       <Filter />
-      <List contacts={getFilteredContacts()} deleteContact={onDeleteContact} />
+      <List />
     </div>
   );
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,34 @@
-
+import React from 'react';
 import PropTypes from 'prop-types';
 import UlList from './List.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContacts } from 'Redux/Contacts/contacts-selector';
+import { getFilter } from 'Redux/Filter/filter-selector';
+import { deleteContact } from 'Redux/Contacts/contacts-slice'; // Виправлено імпорт
 
-export const List = ({ contacts, deleteContact }) => {
+const List = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = contactId => {
+    const action = deleteContact(contactId);
+    dispatch(action);
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   return (
     <UlList>
-      {contacts.map(({ id, name, number }) => (
+      {getFilteredContacts().map(({ id, name, number }) => (
         <li key={id}>
           {name}: {number}
-          <button type="button" onClick={() => deleteContact(id)}>
+          <button type="button" onClick={() => onDeleteContact(id)}>
             Delete
           </button>
         </li>
@@ -19,11 +39,12 @@ export const List = ({ contacts, deleteContact }) => {
 
 List.propTypes = {
   contacts: PropTypes.arrayOf(
-    PropTypes.exact({
+    PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
     })
   ),
-  deleteContact: PropTypes.func.isRequired,
 };
+
+export default List;
